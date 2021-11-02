ea5cbb615bc0d778c25f982cc0b32bbc1913d4e8
Natasha Ortiz <tashaaortiz@gmail.com>
2021-11-01 19:59:06 -0500

    added some boilerplate models for User and some util and auth boiler plate things

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // dependencies
-const express = require("express");
-const routes = require("./routes");
-const sequelize = require("./config/connection");
-require("dotenv").config();
+const express = require('express')
+const routes = require('./routes')
+const sequelize = require('./config/connection')
+require('dotenv').config()
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express()
+const PORT = process.env.PORT || 3001
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(routes);
+app.use(routes)
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
     console.log(`Now listening at http://localhost:${PORT}`)
-  );
-});
+  )
+})
