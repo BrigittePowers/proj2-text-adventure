7db59c78973660032a68668ee3b59cd6e2e7608a
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-11-01 19:27:15 -0500

    Added test model and .json files

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,10 @@
 // TODO: routes
+const router = require('express').Router();
+
+// const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
+
+module.exports = router;
