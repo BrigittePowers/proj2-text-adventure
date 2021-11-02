679b3dcf609824d88f9ead290c0a227eb2d019c9
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-11-01 20:15:36 -0500

    Further testing of local database using sample User data

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-
-// Create a new sequelize store using the express-session package
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-=======
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -20,7 +6,6 @@ const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
->>>>>>> 5bf1efc3a425b30dbb84128386ec160f1ea819ca
 const app = express();
 const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -35,35 +20,6 @@ const sess = {
 	}),
 };
 
-<<<<<<< HEAD
-const hbs = exphbs.create({ helpers });
-
-// Configure and link a session object with the sequelize store
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-// Add express-session and store as Express.js middleware
-app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-=======
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -74,5 +30,4 @@ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log('Now listening'));
->>>>>>> 5bf1efc3a425b30dbb84128386ec160f1ea819ca
 });
