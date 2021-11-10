65d2c4d5f039c3fee5d3ef77f90b8d1a35729c3d
Brigitte Powers PC <reyhn701@gmail.com>
2021-11-09 19:10:01 -0600

    Added story json files and modified models

--- public/js/answer.js
+++ public/js/answer.js
@@ -1,26 +1,25 @@
 const newAnswer = async (event) => {
-    //  CHOICE option has ID attribute
+	//  CHOICE option has ID attribute
 
-    if (event.target.hasAttribute('data-id')) {
-        const choices_id = event.target.getAttribute('data-id');
-        // const choices_id = event.target.getAttribute('id');
+	if (event.target.hasAttribute('data-id')) {
+		console.log('Found answer to create');
+		const choices_id = event.target.getAttribute('data-id');
+		// const choices_id = event.target.getAttribute('id');
 
-        const response = await fetch('/api/answers/', {
-            method: 'POST',
-            body: JSON.stringify({ choices_id }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+		const response = await fetch('/api/answers/', {
+			method: 'POST',
+			body: JSON.stringify({ choices_id }),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
 
-        if (response.ok) {
+		if (response.ok) {
 			document.location.reload;
 		} else {
 			alert('Failed to record answer.');
 		}
-    }
+	}
 };
 
-document
-  .querySelector('.story-choices')
-  .addEventListener('click', newAnswer);
+document.querySelector('.story-choices').addEventListener('click', newAnswer);
